707e08ec9fb11ef7f595748def5fefd14b29b0a7
JassonRM <jassonrm@icloud.com>
2017-11-06 20:56:10 -0600

    Reordenamiento y lista simple C#

---
+++ C#/org.tec.benchmarks/Datastructures benchmark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using org.tec.datastructures;
+
+namespace org.tec.benchmarks
+{
+    public class Benchmark
+    {
+        public static void Main(String[] args){
+            SimpleList<String> list = new SimpleList<String>();
+            list.Append("Hey");
+            list.Append("Hola");
+            list.Append("Que hay");
+            list.Print();
+            Console.WriteLine("-----------------");
+            list.Delete(0);
+            list.Print();
+        }
+    }
+}
---
+++ C#/org.tec.datastructures/SimpleList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+namespace org.tec.datastructures
+{
+    public class SimpleList<T>
+    {
+        Node<T> head;
+        public void Append(T value)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(value);
+            }
+            else
+            {
+                Node<T> nodo = head;
+                while (nodo.GetNext() != null)
+                {
+                    nodo = nodo.GetNext();
+                }
+
+                nodo.SetNext(new Node<T>(value));
+
+            }
+        }
+
+        public void Print()
+        {
+            Node<T> temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.GetValue());
+                temp = temp.GetNext();
+            }
+        }
+
+        public void Delete(int index)
+        {
+            if (index == 0){
+                this.head = this.head.GetNext();
+                return;
+            }
+
+            Node<T> temp = this.head;
+            int counter = 1;
+            while (temp.GetNext() != null)
+            {
+                if (counter == index)
+                {
+                    temp.SetNext(temp.GetNext().GetNext());
+                    return;
+                }else{
+                    temp = temp.GetNext();
+                    counter++;
+                }
+            }
+            Console.WriteLine("No se pudo encontrar el elemento");
+        }
+
+        public T Get(int index)
+        {
+            Node<T> temp = this.head;
+            int counter = 0;
+            while (temp != null)
+            {
+                if (counter == index)
+                {
+                    return temp.GetValue();
+                }
+                else
+                {
+                    temp = temp.GetNext();
+                    counter++;
+                }
+            }
+            return default(T);
+        }
+
+        public class Node<T>
+        {
+            Node<T> next = null;
+            T value;
+
+            public Node(T value)
+            {
+                this.value = value;
+            }
+
+            public Node<T> GetNext()
+            {
+                return next;
+            }
+
+            public void SetNext(Node<T> node)
+            {
+                next = node;
+            }
+
+            public T GetValue()
+            {
+                return this.value;
+            }
+        }
+    }
+}
